5e8ff43443e51aa9dd9798e6e7b3492ddc1d68bb
pdobbins23 <146784496+pdobbins23@users.noreply.github.com>
2025-01-16 19:28:57 -0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// Name: Peter Dobbins
+// Title: ???
+// Date: 16/1/25
+// Time Spent: 0 Hours
+// Mods: ???
+
+"use strict";
+
+let config = {
+	type: Phaser.AUTO,
+	width: 640,
+	height: 480,
+	scene: [Menu, Play],
+};
+
+let game = new Phaser.Game(config);
+
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT;
